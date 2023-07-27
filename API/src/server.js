d0a01d3683b042b77d2f33e99528ab6877edc15c
wesleyBrum <wesleybrum0000@gmail.com>
2023-07-27 12:20:07 +0000

    f

--- API/src/server.js
+++ API/src/server.js
@@ -4,3 +4,6 @@ require('dotenv').config(); // consumindo o dotenv
 const PORT=process.env.PORT || "3000" ; // salvando a port na variavel
 
 app.listen(PORT,() => console.log(`SERVIDOR RODANDO NA PORTA ${PORT}`)); // console 
+git remote add origin https://github.com/WESLEYBRUM/AAPILUX2.git
+git branch -M main
+git push -u origin main
